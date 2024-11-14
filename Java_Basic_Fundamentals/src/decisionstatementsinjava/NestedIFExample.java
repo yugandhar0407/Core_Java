5000d83cb28fa30007b0224816a2052cbd2fa57a
Yugandhar Gampala <129308425+yugandhar0407@users.noreply.github.com>
2024-11-14 14:38:59 +0530

    Update NestedIFExample.java

--- Java_Basic_Fundamentals/src/decisionstatementsinjava/NestedIFExample.java
+++ Java_Basic_Fundamentals/src/decisionstatementsinjava/NestedIFExample.java
@@ -7,7 +7,7 @@ public class NestedIFExample {
 		int age1 = 20;
 		int weight = 30;
 		if(age1 >=18) {
-			if(weight >45) {
+			if(weight > 45) {
 				System.out.println("Person is ready to donate blood");
 			}else {
 				System.out.println("Person is not eligible to donate blood");
